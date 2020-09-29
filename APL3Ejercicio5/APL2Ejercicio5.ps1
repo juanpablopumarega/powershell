3803b37ef28274feed027bc853c9f29b880bda29
Natalia Salerti <nataliasalerti@gmail.com>
2020-09-28 22:35:34 -0300

    Update APL2Ejercicio5.ps1

--- APL3Ejercicio5/APL2Ejercicio5.ps1
+++ APL3Ejercicio5/APL2Ejercicio5.ps1
@@ -1,2 +1,72 @@
-﻿param(
+﻿<#
+.SYNOPSIS
+    Actividad Práctica de Laboratorio Nro: 2 - Primera Entrega
+    Integrantes:
+        # Fernández Durante Cynthya Alexandra   DNI:48693815
+        # López Pumarega Juan Pablo             DNI:34593023
+        # Miranda Andres                        DNI:32972232
+        # Paiva Gordillo Nahuel Alejo           DNI:38455227
+        # Salerti Natalia                       DNI:41559796        
+    
+.DESCRIPTION
+    Reconocer las etiquetas HTML utilizados en la página web que no contengan ningún atributo aria.
+
+.PARAMETER Aria
+    [Requerido] Path absoluto o relativo del archivo que contiene la lista de atributos aria.
+
+.PARAMETER Tags
+    [Requerido] Path absoluto o relativo del archivo que contiene la lista de etiquetas a analizar.
+
+.PARAMETER Web
+    [Requerido] Path absoluto o relativo del archivo HTML a evaluar.
+
+.PARAMETER Out
+    [Requerido] Path absoluto o relativo del archivo de salida.
+    
+
+.EXAMPLE
+
+#>
+
+param([parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({
+                     if(-Not ($_ | Test-Path) ){
+                        throw "No existe el directorio o el File especificado";
+                        }
+                     return $true 
+                     })]
+    [string]$aria,
+
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({
+                     if(-Not ($_ | Test-Path) ){
+                        throw "No existe el directorio o el File especificado";
+                        }
+                     return $true 
+                     })]
+    [string]$tags,
+
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({
+                     if(-Not ($_ | Test-Path) ){
+                        throw "No existe el directorio o el File especificado";
+                        }
+                     return $true 
+                     })]
+    [string]$web,
+
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateScript({
+                     if(-Not ($_ | Test-Path) ){
+                        throw "No existe el directorio o el File especificado";
+                        }
+                     return $true 
+                     })]
+    [string]$out
 )
+
+
