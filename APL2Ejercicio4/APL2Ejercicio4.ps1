cf23bde150b9e1ac47ce9fe3bc270c5d94e899ae
juanpablopumarega <42682453+juanpablopumarega@users.noreply.github.com>
2020-10-15 19:41:15 -0300

    Update APL2Ejercicio4.ps1
    Ajustes en ejemplo de comando de ejecucion.
    Ajuste en el nombre de salida, es MM mayus para indicar el mes

--- APL2Ejercicio4/APL2Ejercicio4.ps1
+++ APL2Ejercicio4/APL2Ejercicio4.ps1
@@ -19,7 +19,7 @@
     [Opcional] Path absoluto o relativo del directorio donde se generará el archivo de salida. Si no se informa se generará en el directorio de ejecución
 
 .EXAMPLE
-    .\APL2Ejercicio4.ps1 -Blacklist ".\files\blacklist" -Resultado ".\files\"
+    .\APL2Ejercicio4.ps1 -Blacklist ".\files\blacklist.txt" -Resultado ".\files\"
 
 #>
 
@@ -48,51 +48,50 @@ Param(
 
 
 # Funcion que lee el archivo de blacklist y escribe el archivo de salida.
-function global:Blacklist-Reader ()
-{
-  Process{
+    function global:Blacklist-Reader ()
+    {
+      Process{
         
-        $BlackContent = Get-Content "$Global:Blacklist"
-        # Cargo y recorro un array con los nombres de los procesos a cerrar, y los envio a la funcion Detener-Proceso
-        foreach( $proc in $BlackContent ){
+            $BlackContent = Get-Content "$Global:Blacklist"
+            # Cargo y recorro un array con los nombres de los procesos a cerrar, y los envio a la funcion Detener-Proceso
+            foreach( $proc in $BlackContent ){
 
-                # Guardo en un archivo como lista, fecha de creacion, pid, nombre                
-                Get-Process -Name $proc -IncludeUserName | Select-Object Name, UserName, StartTime, Id| Format-Table >> $Global:OutputFileName
+                    # Guardo en un archivo como lista, fecha de creacion, pid, nombre                
+                    Get-Process -Name $proc -IncludeUserName | Select-Object Name, UserName, StartTime, Id| Format-Table >> $Global:OutputFileName
 
-                # Envio los nombres de los procesos que se encontraron corriendo
-                get-process -Name $proc | Select-Object -Unique -Property ProcessName #| global:Detener-Proceso
-                Stop-Process -Name $proc -Force
+                    # Envio los nombres de los procesos que se encontraron corriendo
+                    get-process -Name $proc | Select-Object -Unique -Property ProcessName
+
+                    # Se detiene el proceso
+                    Stop-Process -Name $proc -Force
                  
-            }      
-            
-  }
-} 
+                }         
+      }
+    } 
 
 
 
 
 # Declaracion de timer para que el proceso ejecute cada 10 segundos (10000 ms).
-$Timer = New-Object -Type Timers.Timer
-$Timer.Interval  = 10000
+    $Timer = New-Object -Type Timers.Timer
+    $Timer.Interval  = 10000
 
 # Declaracion de las variables como globales para que se puedan ver desde el entorno de las funciones.
 # Se verifica que el archivo de Blacklist no esté vacío para de ser así, cerrar el proceso y no efectuar tareas.
-if(Get-Content $Blacklist) {
-    $Global:blacklist= $Blacklist
-}
-else {
-    Write-Host "El archivo blacklist está vacío."
-    exit 0;
-}
+    if(Get-Content $Blacklist) {
+        $Global:blacklist= $Blacklist
+    }
+    else {
+        Write-Host "El archivo blacklist está vacío."
+        exit 0;
+    }
 
 # Formo la ruta del archivo de salida.
-$Global:OutputFileName = $Resultado + "\" + "blacklist" + "_" + (Get-Date -Format yyyy-mm-dd_hhmmss) + ".out";
-
-
+    $Global:OutputFileName = $Resultado + "\" + "blacklist" + "_" + (Get-Date -Format yyyy-MM-dd_hhmmss) + ".out";
 
 # Inicio del programa a traves del registro del evento, ambas funciones son globales para que existan en el scope global
-Register-ObjectEvent -InputObject $Timer  -EventName Elapsed  -SourceIdentifier TimerEvent  -Action { global:Blacklist-Reader
-}
-$Timer.Start()
+    Register-ObjectEvent -InputObject $Timer  -EventName Elapsed  -SourceIdentifier TimerEvent  -Action { global:Blacklist-Reader
+    }
+    $Timer.Start()
 
 #FIN
