f7e27d0bc469f184fb15b6c62147e2d47184a00d
juanpablopumarega <42682453+juanpablopumarega@users.noreply.github.com>
2021-01-23 10:53:00 -0300

    Update APL2Ejercicio3.ps1

--- APL2Ejercicio3/APL2Ejercicio3.ps1
+++ APL2Ejercicio3/APL2Ejercicio3.ps1
@@ -1,13 +1,6 @@
 ﻿<#
 .SYNOPSIS
-    Actividad Práctica de Laboratorio Nro: 2 - Primera Entrega
-    Ejercicio Nro: 3.
-    Integrantes:
-        # Fernández Durante Cynthya Alexandra   DNI:48693815
-        # López Pumarega Juan Pablo             DNI:34593023
-        # Miranda Andres                        DNI:32972232
-        # Paiva Gordillo Nahuel Alejo           DNI:38455227
-        # Salerti Natalia                       DNI:41559796        
+    Actividad Práctica de Laboratorio Nro: 2 - Primera Entrega        
     
 .DESCRIPTION
     Determinar la existencia de archivos duplicados en el File System y cuáles archivos superan en tamaño un umbral determinado.
